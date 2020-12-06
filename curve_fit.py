829ea50db428cf3c95164a2984410ddb757bbe0c
sperli14 <46566726+sperli14@users.noreply.github.com>
2020-12-06 14:33:49 -0500

    Updated curve fit

--- curve_fit.py
+++ curve_fit.py
@@ -15,7 +15,7 @@ def curve_fit_network_in(filename, start_index, end_index):
     y=[]
     colors=[]
     for i, line in enumerate(fp):
-        if i == 1:#time
+        if i == 0:#time
             x = line.split(',')[1:]
             x = x[start_index:end_index]
             x = [float(a) for a in x]
@@ -23,18 +23,20 @@ def curve_fit_network_in(filename, start_index, end_index):
             colors = line.split(',')[1:]
             colors = colors[start_index:end_index]
             colors = [float(a) for a in colors]
-        elif i == 7:#network in
+        elif i == 6:#network in
             y = line.split(',')[1:]
             y = y[start_index:end_index]
-            y = [float(a) for a in y]
+            y = [float(a) for a in y] #convert to megabytes
     x = np.array(x)
     y = np.array(y)
+    print(x)
+    print(y)
     
     popt, pcov = curve_fit(fitted_curve, x, y)
     
     print("Order of function:",*popt)
     
-    plt.figure()
+    #plt.figure(figsize=(5,10))
     #plt.plot(x, y, "ko", label="Data")
     plt.scatter(x, y, c=colors, label="Data")
     plt.plot(x, fitted_curve(x, *popt), 'r-', label="Fitted Curve")
@@ -47,7 +49,7 @@ def curve_fit_cpu(filename, start_index, end_index):
     y=[]
     colors=[]
     for i, line in enumerate(fp):
-        if i == 1:#time
+        if i == 0:#time
             x = line.split(',')[1:]
             x = x[start_index:end_index]
             x = [float(a) for a in x]
@@ -55,7 +57,7 @@ def curve_fit_cpu(filename, start_index, end_index):
             colors = line.split(',')[1:]
             colors = colors[start_index:end_index]
             colors = [float(a) for a in colors]
-        elif i == 6:#cpu
+        elif i == 5:#cpu
             y = line.split(',')[1:]
             y = y[start_index:end_index]
             y = [float(a) for a in y]
@@ -74,6 +76,6 @@ def curve_fit_cpu(filename, start_index, end_index):
     plt.show()
 
 #curve_fit_network_in("data/robot_ball.csv", 38, 56)
-#curve_fit_cpu("data/robot_ball.csv", 38, 56)
-curve_fit_network_in("data/unity_dots.csv", 100, 200)
+curve_fit_cpu("data/robot_ball.csv", 38, 56)
+#curve_fit_network_in("data/unity_dots.csv", 100, 200)
 #curve_fit_cpu("data/unity_dots.csv", 100, 200)
