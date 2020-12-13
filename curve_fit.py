80b057af67dd833051462f7ecd1d24747d84d4a5
sperli14 <46566726+sperli14@users.noreply.github.com>
2020-12-13 18:15:10 -0500

    added images

--- curve_fit.py
+++ curve_fit.py
@@ -6,8 +6,8 @@ from scipy.optimize import curve_fit
 
 #Graphing code from https://stackoverflow.com/questions/3433486/how-to-do-exponential-and-logarithmic-curve-fitting-in-python-i-found-only-poly
 
-def fitted_curve(x, a):
-    return x**a
+def fitted_curve(x, a, b):
+    return x*a + b
 
 def curve_fit_network_in(filename, start_index, end_index):
     fp = open(filename)
@@ -18,7 +18,7 @@ def curve_fit_network_in(filename, start_index, end_index):
         if i == 0:#time
             x = line.split(',')[1:]
             x = x[start_index:end_index]
-            x = [float(a) for a in x]
+            x = [float(a)-39 for a in x]
         elif i == 2: #number of players
             colors = line.split(',')[1:]
             colors = colors[start_index:end_index]
@@ -38,6 +38,47 @@ def curve_fit_network_in(filename, start_index, end_index):
     
     #plt.figure(figsize=(5,10))
     #plt.plot(x, y, "ko", label="Data")
+    
+    plt.xlabel("Time (min)")
+    plt.ylabel("Network In (bytes)")
+    plt.scatter(x, y, c=colors, label="Data")
+    plt.plot(x, fitted_curve(x, *popt), 'r-', label="Fitted Curve")
+    plt.legend()
+    plt.show()
+    
+def curve_fit_network_out(filename, start_index, end_index):
+    fp = open(filename)
+    x=[]
+    y=[]
+    colors=[]
+    for i, line in enumerate(fp):
+        if i == 0:#time
+            x = line.split(',')[1:]
+            x = x[start_index:end_index]
+            #x = [float(a)-39 for a in x]
+            x = [float(a)-39 for a in x]
+        elif i == 2: #number of players
+            colors = line.split(',')[1:]
+            colors = colors[start_index:end_index]
+            colors = [float(a) for a in colors]
+        elif i == 7:#network out
+            y = line.split(',')[1:]
+            y = y[start_index:end_index]
+            y = [float(a) for a in y] #convert to megabytes
+    x = np.array(x)
+    y = np.array(y)
+    print(x)
+    print(y)
+    
+    popt, pcov = curve_fit(fitted_curve, x, y)
+    
+    print("Order of function:",*popt)
+    
+    #plt.figure(figsize=(5,10))
+    #plt.plot(x, y, "ko", label="Data")
+    
+    plt.xlabel("Time (min)")
+    plt.ylabel("Network Out (bytes)")
     plt.scatter(x, y, c=colors, label="Data")
     plt.plot(x, fitted_curve(x, *popt), 'r-', label="Fitted Curve")
     plt.legend()
@@ -52,7 +93,7 @@ def curve_fit_cpu(filename, start_index, end_index):
         if i == 0:#time
             x = line.split(',')[1:]
             x = x[start_index:end_index]
-            x = [float(a) for a in x]
+            x = [float(a)-39 for a in x]
         elif i == 2: #number of players
             colors = line.split(',')[1:]
             colors = colors[start_index:end_index]
@@ -70,12 +111,17 @@ def curve_fit_cpu(filename, start_index, end_index):
     
     plt.figure()
     #plt.plot(x, y, 'ko', label="Data")
+    plt.xlabel("Time (min)")
+    plt.ylabel("CPU Usage (%)")
     plt.scatter(x, y, c=colors, label="Data")
     plt.plot(x, fitted_curve(x, *popt), 'r-', label="Fitted Curve")
     plt.legend()
     plt.show()
 
-#curve_fit_network_in("data/robot_ball.csv", 38, 56)
-curve_fit_cpu("data/robot_ball.csv", 38, 56)
-#curve_fit_network_in("data/unity_dots.csv", 100, 200)
-#curve_fit_cpu("data/unity_dots.csv", 100, 200)
+#curve_fit_network_in("data/robot_ball.csv", 38, 53)
+curve_fit_cpu("data/robot_ball.csv", 38, 53)
+#curve_fit_network_out("data/robot_ball.csv", 38, 53)
+#curve_fit_network_in("data/unity_dots2.csv", 162, 167)
+#curve_fit_cpu("data/unity_dots2.csv", 162, 167)
+
+#curve_fit_network_out("data/unity_dots2.csv", 162, 167)
